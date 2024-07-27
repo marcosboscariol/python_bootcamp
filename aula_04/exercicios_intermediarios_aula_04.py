7493e529c53ca14992c1c08b463eab7042e9e2ed
Marcos Boscariol <118377554+marcosboscariol@users.noreply.github.com>
2024-07-27 15:36:11 -0300

    adicionar arquivo

---
+++ aula_04/exercicios_intermediarios_aula_04.py
@@ -0,0 +1,8 @@
+# Eliminação de Duplicatas: Objetivo: Dada uma lista de emails, remover todos os duplicados.
+
+emails = ["a@a.com", "a@a.com", "b@a.com"]
+
+print(len(emails))
+
+for i in emails:
+    print(i)
