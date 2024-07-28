cb8ea56dfd58183ba59a889cc5139cec62e04adb
Marcos Boscariol <118377554+marcosboscariol@users.noreply.github.com>
2024-07-27 21:41:14 -0300

    incluir a resolucao dos exercicios 10 a 15

--- aula_04/exercicios_intermediarios_aula_04.py
+++ aula_04/exercicios_intermediarios_aula_04.py
@@ -53,3 +53,72 @@ print(f"Valores pares {valores_pares}")
 
 print(f"Valores impares {valores_impares}")
 """
+
+# Atualização de Dados: Objetivo: Dada uma lista de dicionários representando produtos, atualizar o preço de um produto específico.
+
+"""
+produtos = [
+    {"id": 1, "nome": "Teclado", "preço": 100},
+    {"id": 2, "nome": "Mouse", "preço": 80},
+    {"id": 3, "nome": "Monitor", "preço": 300}
+]
+
+for i in produtos:
+    if i["id"] == 1:
+        i["preço"] = 90
+
+print(produtos)
+"""
+
+# Fusão de Dicionários: Objetivo: Dados dois dicionários, fundi-los em um único dicionário.
+
+"""
+dicionario1 = {"a": 1, "b": 2}
+dicionario2 = {"c": 3, "d": 4}
+
+dicionario3 = {**dicionario1, **dicionario2}
+
+print(dicionario3)
+
+dicionario4 = dicionario1
+
+dicionario4.update(dicionario2)
+
+print(dicionario4)
+"""
+
+# Filtragem de Dados em Dicionário: Objetivo: Dado um dicionário de estoque de produtos, filtrar aqueles com quantidade maior que 0.
+
+"""
+estoque = {"Teclado": 10, "Mouse": 0, "Monitor": 3, "CPU": 0}
+
+estoque_positivo = {
+    produto: quantidade for produto, quantidade in estoque.items() if quantidade > 0}
+
+print(estoque_positivo)
+"""
+
+# Extração de Chaves e Valores: Objetivo: Dado um dicionário, criar listas separadas para suas chaves e valores.
+
+"""
+dicionario = {"a": 1, "b": 2, "c": 3}
+chaves = list(dicionario.keys())
+valores = list(dicionario.values())
+
+print("Chaves:", chaves)
+print("Valores:", valores)
+print(dicionario.keys())
+"""
+
+# Contagem de Frequência de Itens: Objetivo: Dada uma string, contar a frequência de cada caractere usando um dicionário.
+
+texto = "engenharia de dados"
+frequencia = {}
+
+for caractere in texto:
+    if caractere in frequencia:
+        frequencia[caractere] += 1
+    else:
+        frequencia[caractere] = 1
+
+print(frequencia)
