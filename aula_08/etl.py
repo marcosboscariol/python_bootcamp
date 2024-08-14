41d15cac6c7507162df697529e016ca30920fba0
Marcos Boscariol <118377554+marcosboscariol@users.noreply.github.com>
2024-08-14 13:19:14 -0300

    adding def extrair_dados

--- aula_08/etl.py
+++ aula_08/etl.py
@@ -1,2 +1,13 @@
 import pandas as pd
-import pandera as pand
+import os
+import glob
+
+pasta = 'aula_08\data'
+
+
+def extrair_dados(pasta: str) -> pd.DataFrame:
+
+    arquivos_json = glob.glob(os.path.join(pasta, '*.json'))
+    df_list = [pd.read_json(arquivo) for arquivo in arquivos_json]
+    df_total = pd.concat(df_list, ignore_index=True)
+    print(df_total)
