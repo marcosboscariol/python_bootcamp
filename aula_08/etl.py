14de0a2379fa555182e1a26c4581a601c0e3d12b
Marcos Boscariol <118377554+marcosboscariol@users.noreply.github.com>
2024-08-14 13:31:30 -0300

    adding def calcular_kpi_total_de_vendas

--- aula_08/etl.py
+++ aula_08/etl.py
@@ -2,12 +2,20 @@ import pandas as pd
 import os
 import glob
 
-pasta = 'aula_08\data'
 
-
-def extrair_dados(pasta: str) -> pd.DataFrame:
+def extrair_dados_e_consolidar(pasta: str) -> pd.DataFrame:
 
     arquivos_json = glob.glob(os.path.join(pasta, '*.json'))
     df_list = [pd.read_json(arquivo) for arquivo in arquivos_json]
     df_total = pd.concat(df_list, ignore_index=True)
-    print(df_total)
+    return df_total
+
+
+def calcular_kpi_total_de_vendas(df: pd.DataFrame) -> pd.DataFrame:
+    df["Total"] = df["Quantidade"] * df["Venda"]
+    return df
+
+
+pasta_argumento = 'aula_08\data'
+data_frame = extrair_dados_e_consolidar(pasta=pasta_argumento)
+print(calcular_kpi_total_de_vendas(data_frame))
