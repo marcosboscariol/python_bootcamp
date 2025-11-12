6d5e97299605c6af6d8dc227692da726ec3096ef
Macbook pro <macbookpro@MacBook-Pro-de-Macbook.local>
2025-11-12 17:20:50 -0300

    add file exemplo01

---
+++ aula_01/exemplo01.py
@@ -0,0 +1,6 @@
+# Crie um programa que o usuário digita o seu nome e retorna a quantidade de caracteres
+
+user_name = input('Digite seu nome: ')
+user_name_len = len(user_name)
+
+print(f'O nome {user_name} tem {user_name_len} caracteres')
